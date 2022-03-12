e9db4176f4fdaafc5386dbb152c7997964662f51
Ryo <r.tachikawa@outlook.jp>
2022-03-12 12:25:36 +0000

    CRUD,完成

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,56 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+
+    if @task.save
+      flash[:success] = 'Success!'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'Failed'
+      render :new
+    end
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+
+    if @task.update(task_params)
+      flash[:success] = 'Success!'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'Failed...'
+      render :edit
+    end
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+
+    flash[:success] = '正常に削除されました'
+    redirect_to tasks_url
+  end
+  
+  private
+
+  # Strong Parameter
+  def task_params
+    params.require(:task).permit(:content)
+  end
+end
